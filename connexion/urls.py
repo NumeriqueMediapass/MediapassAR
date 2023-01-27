fd204d081d66f3aa52b3cbdbb5f5e863ba5123f3
ElDiosAgua <anthony.williame@uphf.fr>
2023-01-27 11:04:07 +0100

    Ajout d'un nouveau dossier
    Ajout d'un nouveau dossier afin de séparer la partie connexion/ inscriptions du site web

--- connexion/urls.py
+++ connexion/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import signup, login_view, acceuil, logout_view
+from .views import signup, login_view,logout_view
 
 urlpatterns = [
     path('signup/', signup, name='signup'),
     path('login/', login_view, name='login'),
-    path('',acceuil, name='acceuil'),
     path('logout/', logout_view, name='logout'),
 ]
