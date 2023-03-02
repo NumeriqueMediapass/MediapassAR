b43b6541d57c73bce3f05b1948643d9cd13a6d06
ElDiosAgua <anthony.williame@uphf.fr>
2023-03-02 11:50:01 +0100

    Update correction bug

--- administration/urls.py
+++ administration/urls.py
@@ -5,12 +5,12 @@ from .views import index, UsersViews, UsersEdit, UsersCreation, UserDeletion, Me
 urlpatterns = [
     path('admininstration/', index, name='index_admin'),
     path('admininstration/users/', UsersViews, name='UsersViews'),
-    path('admininstration/edit_user/<int:id>', UsersEdit, name='UsersEdit'),
+    path('admininstration/edit_user/<int:idusers>', UsersEdit, name='UsersEdit'),
     path('admininstration/create_user/', UsersCreation, name='UsersCreation'),
     path('admininstration/delete_users/', UserDeletion, name='UserDeletion'),
     path('admininstration/create_mediatheque/', MediaLibraryCreations, name='MediaLibraryCreations'),
     path('admininstration/mediatheques/', MediaLibraryViews, name='MediaLibraryViews'),
-    path('admininstration/edit_mediatheque/<int:id>', MediaLibraryEditing, name='MediaLibraryEditing'),
+    path('admininstration/edit_mediatheque/<int:idmedialibrary>', MediaLibraryEditing, name='MediaLibraryEditing'),
     path('admininstration/delete_mediatheque/', MediaLibraryDelation, name='MediaLibraryDelation'),
 
 ]
